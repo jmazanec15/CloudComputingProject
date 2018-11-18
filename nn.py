ae8331ee2b1090d11bb4e2e37aa09eee0cd9baef
Jack Mazanec <jmazane1@nd.edu>
2018-11-18 17:14:38 -0500

    updated nn

--- nn.py
+++ nn.py
@@ -13,32 +13,19 @@ class NN(object):
 	def create_model(self):
 		'''
 			This method should create the deep neural net
-			It should take the game state as the input,
-			then stack several residual/convulutional layers
+			It should stack several residual/convulutional layers
 			on top of each and then split off into the policy_head and the
 			value_head
 		'''
-		## Main input Shape to the model
 		inputs = Input(shape=self.input_shape, dtype='int32', name='main_input')
 
 		## Base of the model before split
-		x = LeakyReLU()(inputs)
-		x = Dense(64, activation='relu')(x)
-		x = Dense(64, activation='relu')(x)
-		x = Dense(64, activation='relu')(x)
+		base = self.model_base(inputs)
 
-		## Next, generate the value_head and the policy_head
-		p = Dense(64, activation='relu')(x)
-		p = Dense(64, activation='relu')(p)
-		p = Dense(64, activation='relu')(p)
-		policy_head = Dense(42, activation='softmax')(p)
+		## Split net into value_head and the policy_head
+		policy_head = self.policy_head(base)
+		value_head = self.value_head(base)
 
-		v = Dense(64, activation='relu')(x)
-		v = Dense(64, activation='relu')(v)
-		v = Dense(64, activation='relu')(v)
-		value_head = Dense(1, activation='softmax')(v)
-
-		## Creat the model and compile it
 		model = Model(inputs=inputs, outputs=[policy_head, value_head])
 		model.compile(optimizer='rmsprop', loss=['categorical_crossentropy', 'binary_crossentropy'], metrics=['accuracy'])
 
@@ -51,7 +38,17 @@ class NN(object):
 	def evaluate(self, x, y, batch_size):
 		return self.model.evaluate(x, y, batch_size=batch_size)
 
-	def policy_head(self):
+	def predict(self, data):
+		return self.model.predict(data)
+
+	def model_base(self, inputs):
+		x = LeakyReLU()(inputs)
+		x = Dense(64, activation='relu')(x)
+		x = Dense(64, activation='relu')(x)
+		x = Dense(64, activation='relu')(x)
+		return x
+
+	def policy_head(self, x):
 		'''
 			This head will create a probability vector of all of the
 			moves
@@ -60,14 +57,22 @@ class NN(object):
 		#	1.) If there are a fixed number of outputs, what happens to invalid moves
 		#			-> I am guessing they just go to 0
 
-		pass
+		p = Dense(64, activation='relu')(x)
+		p = Dense(64, activation='relu')(p)
+		p = Dense(64, activation='relu')(p)
+		policy_head = Dense(42, activation='softmax')(p)
+		return policy_head
 
-	def value_head(self):
+	def value_head(self, x):
 		'''
 			This head will have 1 output node of how favorable the board is for the user
 			between -1 and 1
 		'''
-		pass
+		v = Dense(64, activation='relu')(x)
+		v = Dense(64, activation='relu')(v)
+		v = Dense(64, activation='relu')(v)
+		value_head = Dense(1, activation='softmax')(v)
+		return value_head
 
 
 		
