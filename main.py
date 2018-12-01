8aedbe8a504ad2c4b37c6a14dfb3fba87bc84e84
Luke Wurl <lwurl@nd.edu>
2018-11-30 19:14:19 -0500

    Change main.py

--- main.py
+++ main.py
@@ -1,10 +1,104 @@
-#!/usr/bin/env python
+#!/usr/bin/env cctools_python
+# CCTOOLS_PYTHON_VERSION 2.7 2.6
 
 import numpy as np
+import subprocess
+import time
 
-from connect4 import Connect4
+from work_queue import *
+from games.connect4 import Connect4
 from nn import NN
 from agent import ComputerAgent, HumanAgent
+
+from work_queue import *
+
+def workers(games_per_task, games_per_iter):
+	port = 9139
+
+	keras_path = "/afs/crc.nd.edu/user/l/lwurl/.local/lib/python2.7/site-packages/keras"
+	h5py_path = "/afs/crc.nd.edu/user/l/lwurl/.local/lib/python2.7/site-packages/h5py"
+	keras_applications_path = "/afs/crc.nd.edu/user/l/lwurl/.local/lib/python2.7/site-packages/keras_applications"
+	keras_preprocessing_path = "/afs/crc.nd.edu/user/l/lwurl/.local/lib/python2.7/site-packages/keras_preprocessing"
+	yaml_path = "/afs/crc.nd.edu/user/l/lwurl/.local/lib/python2.7/site-packages/yaml"
+	numpy_path = "/afs/crc.nd.edu/x86_64_linux/t/tensorflow/1.6/gcc/python2/build/lib/python2.7/site-packages/numpy"
+	script_path = "/afs/crc.nd.edu/user/l/lwurl/Cloud/script.sh"
+	cloud_path = "/afs/crc.nd.edu/user/l/lwurl/Cloud"
+
+	try:
+		q = WorkQueue(port)
+	except:
+		sys.exit(1)
+
+	print "listening on port %d..." % q.port
+
+	for i in range(games_per_iter/games_per_task):
+		train_file = 'training_examples' + str(i) + '.npy'
+		policies_file = 'policies' + str(i) + '.npy'
+		values_file = 'values' + str(i) + '.npy'
+		outfile = "errors.txt"
+
+		command = "./script.sh " + str(games_per_task) + " " + str(i) + " >> %s 2>&1" % (outfile)
+
+		t = Task(command)
+
+		t.specify_file(keras_path, "keras", WORK_QUEUE_INPUT, cache=True)
+		t.specify_file(h5py_path, "h5py", WORK_QUEUE_INPUT, cache=True)
+		t.specify_file(cloud_path, "cloud", WORK_QUEUE_INPUT, cache=True)
+		t.specify_file(keras_applications_path, "keras_applications", WORK_QUEUE_INPUT, cache=True)
+		t.specify_file(keras_preprocessing_path, "keras_preprocessing", WORK_QUEUE_INPUT, cache=True)
+		t.specify_file(yaml_path, "yaml", WORK_QUEUE_INPUT, cache=True)
+		t.specify_file(numpy_path, "numpy", WORK_QUEUE_INPUT, cache=True)
+		t.specify_file(script_path, "script.sh", WORK_QUEUE_INPUT, cache=True)
+		
+		t.specify_file(train_file, train_file, WORK_QUEUE_OUTPUT, cache=False)
+		t.specify_file(policies_file, policies_file, WORK_QUEUE_OUTPUT, cache=False)
+		t.specify_file(values_file, values_file, WORK_QUEUE_OUTPUT, cache=False)
+		t.specify_file(outfile, outfile, WORK_QUEUE_OUTPUT, cache=False)
+
+		# Once all files has been specified, we are ready to submit the task to the queue.
+		taskid = q.submit(t)
+		print "submitted task (id# %d): %s" % (taskid, t.command)
+
+	print "waiting for tasks to complete..."
+	while not q.empty():
+		t = q.wait(5)
+		if t:
+			print "task (id# %d) complete: %s (return code %d)" % (t.id, t.command, t.return_status)
+			if t.return_status != 0:
+				i = t.id - 1
+				train_file = 'training_examples' + str(i) + '.npy'
+				policies_file = 'policies' + str(i) + '.npy'
+				values_file = 'values' + str(i) + '.npy'
+				outfile = "errors.txt"
+
+				command = "./script.sh " + str(games_per_task) + " " + str(i) + " >> %s 2>&1" % (outfile)
+
+				t = Task(command)
+
+				t.specify_file(keras_path, "keras", WORK_QUEUE_INPUT, cache=True)
+				t.specify_file(h5py_path, "h5py", WORK_QUEUE_INPUT, cache=True)
+				t.specify_file(cloud_path, "cloud", WORK_QUEUE_INPUT, cache=True)
+				t.specify_file(keras_applications_path, "keras_applications", WORK_QUEUE_INPUT, cache=True)
+				t.specify_file(keras_preprocessing_path, "keras_preprocessing", WORK_QUEUE_INPUT, cache=True)
+				t.specify_file(yaml_path, "yaml", WORK_QUEUE_INPUT, cache=True)
+				t.specify_file(numpy_path, "numpy", WORK_QUEUE_INPUT, cache=True)
+				t.specify_file(script_path, "script.sh", WORK_QUEUE_INPUT, cache=True)
+				
+				t.specify_file(train_file, train_file, WORK_QUEUE_OUTPUT, cache=False)
+				t.specify_file(policies_file, policies_file, WORK_QUEUE_OUTPUT, cache=False)
+				t.specify_file(values_file, values_file, WORK_QUEUE_OUTPUT, cache=False)
+				t.specify_file(outfile, outfile, WORK_QUEUE_OUTPUT, cache=False)
+
+				# Once all files has been specified, we are ready to submit the task to the queue.
+				taskid = q.submit(t)
+				print "REsubmitted task (id# %d): %s" % (taskid, t.command)
+				#task object will be garbage collected by Python automatically when it goes out of scope
+
+	print "all tasks complete!"
+
+	#work queue object will be garbage collected by Python automatically when it goes out of scope
+	
+	return "ok"
 
 def main():
 	'''
@@ -14,29 +108,41 @@ def main():
 	### Parameters
 	input_shape = (6, 7, 1)
 	iterations = 10
-	games_per_iter = 100
-	head_to_head_games = 50
+	games_per_iter = 1000	# 100
+	head_to_head_games = 5	# 50
 	threshold = 0.55
 	cpuct = 1
-	epochs = 10
+	epochs = 2	# 10
 	batch_size = 64
 	training_size = 10000
 
 	### Setup variables
 	nn = NN(input_shape)
-	nn.save_model('./models/v1.h5')
 	game = Connect4()
 	training_examples = np.array([])
 	policies = list()
 	values = list()
+
+	games_per_task = 5
+
+	f = open("{}_games_per_iter_100workers_{}_games_per_task.csv".format(games_per_iter, games_per_task), "a")
 
 	for it in range(iterations):
 		print("ITERATION: {}/{}".format(it+1, iterations))
 		
 		### Self playing to accumulate training examples
 		print("Self playing {} games".format(games_per_iter))
-		for g in range(games_per_iter):
-			new_examples, ps, vs = playGameSelfVsSelf(game, nn, cpuct)
+
+		ti = time.time()
+
+		workers(games_per_task, games_per_iter)
+		for i in range(games_per_iter/games_per_task):
+			train_file = 'training_examples' + str(i) + '.npy'
+			policies_file = 'policies' + str(i) + '.npy'
+			values_file = 'values' + str(i) + '.npy'
+			new_examples = np.load(train_file)
+			ps = np.load(policies_file)
+			vs = np.load(values_file)
 			if len(training_examples) != 0:
 				training_examples = np.append(training_examples, new_examples, axis=0)
 				policies = np.append(policies, ps, axis=0)
@@ -46,6 +152,11 @@ def main():
 				policies = ps
 				values = vs
 
+		tf = time.time()
+
+		it_time = tf-ti
+		f.write(str(it_time))
+		f.write('\n')
 
 		### Train new neural net with random assortment of new examples
 		print("Training...")
@@ -69,9 +180,9 @@ def main():
 				wins+=1
 		
 		if wins/float(head_to_head_games) >= threshold:
-			nn = new_nn 
+			nn = new_nn
 
-	#playGameHumanVsComp(game, nn, cpuct)
+	playGameHumanVsComp(game, nn, cpuct)
 
 	nn.save_model('./models/v1.h5')
 
@@ -172,10 +283,10 @@ def test_game(game):
 		a = game.getValidActions(s)
 		s = game.nextState(s, a[0])
 		game.printState(s)
-		print ''
+		print('')
 		t = game.gameOver(s)
 		if t:
-			print "{} wins".format(t)
+			print("{} wins".format(t))
 			break
 
 	print(s)
