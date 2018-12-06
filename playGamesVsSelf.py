2bc5044922b51b4f461b3ecbcd905678d4df830d
Jack Mazanec <jmazane1@nd.edu>
2018-12-06 01:05:06 -0500

    added a change to neural network

--- playGamesVsSelf.py
+++ playGamesVsSelf.py
@@ -53,7 +53,7 @@ def main():
 				a, p = a2.getMove(s, get_policy=True)
 				a1_turn = True
 
-			examples.append(s)
+			examples.append(game.convertStateForNN(s))
 			ps.append(p)
 
 			s = game.nextState(s, a)
